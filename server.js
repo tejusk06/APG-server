31dfdb39247df0ba29dc9869d3a83870855ac05e
tejusk06 <tejus.kabadi@gmail.com>
2022-05-08 05:02:53 +0530

    added the classes code

--- server.js
+++ server.js
@@ -5,15 +5,53 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+// ${process.env.AIRTABLE_KEY}
+
+// Configuring Airtable
+var Airtable = require("airtable");
+Airtable.configure({
+  endpointUrl: "https://api.airtable.com",
+  apiKey: `${process.env.AIRTABLE_KEY}`,
+});
+var base = Airtable.base("appvnq3LlzxDIHTqI");
 
 // Get class for a
-app.get("/api/v1/classes/:studentId", (req, res) => {
-  res
-    .status(200)
-    .json({
-      success: true,
-      msg: ` This gets all the classes for ${req.params.studentId} from ${process.env.AIRTABLE_KEY}`,
-    });
+app.get("/api/v1/classes/", (req, res) => {
+  base("Classes")
+    .select({
+      // Selecting the first 3 records in Grid view:
+      maxRecords: 10,
+      view: "Grid view",
+    })
+    .eachPage(
+      function page(records, fetchNextPage) {
+        // This function (`page`) will get called for each page of records.
+
+        console.log(records);
+
+        res.status(200).json({
+          success: true,
+          msg: ` This gets all the classes for ${req.params.studentId}`,
+          classes: records,
+        });
+
+        records.forEach(function (record) {
+          console.log("Retrieved", record.get("Name"));
+        });
+
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        fetchNextPage();
+      },
+      function done(err) {
+        console.log("done called");
+        if (err) {
+          console.error(err);
+          return;
+        }
+      }
+    );
 });
 
 // Update a class for student
