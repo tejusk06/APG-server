7522efbbd943d260586778b9f40927b5be648485
Tejus Kabadi <tejuskabadi@Tejuss-MacBook-Pro.local>
2022-05-08 01:12:03 +0530

    changed url

--- server.js
+++ server.js
@@ -6,6 +6,25 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+// Get class for a
+app.get("/", (req, res) => {
+  res.status(200).json({ success: true, msg: ` This gets all the classes for ${req.params.studentId}` });
+});
+
+// Update a class for student
+/*
+app.put("/api/v1/classes/:studentId", (req, res) => {
+  res.status(200).json({ success: true, msg: `${req.params.studentId}` });
+});
+*/
+
+// Route to create records
+/*
+app.post("/api/v1/classes/:studentId", (req, res) => {
+  res.status(200).json({ success: true, msg: `${req.params.studentId}` });
+});
+*/
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
